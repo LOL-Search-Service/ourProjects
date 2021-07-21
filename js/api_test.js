02ad70acd6b2e4f42a1b057b72fbbe87ad31d740
leesuun <77526673+leesuun@users.noreply.github.com>
2021-07-21 23:10:01 +0900

    1

---
+++ js/api_test.js
@@ -0,0 +1,4 @@
+const API_KEY = "RGAPI-5cfb3d92-d58d-4023-ab45-9f48fe862b74";
+
+const url = `https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/지노스프?api_key=${API_KEY}`;
+console.log(url);
